7375523e523c37589f0e95cd10c1ce3baa323728
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-12-14 19:49:44 +0600

    add vercel json

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
-    const orderCollection = client.db("Hospisearch").collection("orders");
+    const orderCollection = client.db("HospiSearch").collection("orders");
     // perform actions on the collection object
 
     // Initialize app
